6c436bacb1d3d0fc5712d68f8e7578cfb17f6bab
samposalmela <samposalmela111@gmail.com>
2022-01-10 19:29:09 +0200

    Completed Day 1 project

---
+++ day-1-band-name-generator/main.py
@@ -0,0 +1,5 @@
+#Combines the name of the city you input and the pet name you input to create a name for a band
+print("Welcome to the band name generator!")
+city = input("What was the city you were born in? \n")
+pet = input("What is the name of a pet? \n")
+print("Your band name could be " + city + " " + pet)
